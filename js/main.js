3a397f819ea7a1e7eed7292665c25410c4b622f5
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-06 17:49:06 +0200

    add input elements and style

--- js/main.js
+++ js/main.js
@@ -5,24 +5,39 @@
 
 // Inizializazione variabili
 var numToMemorize = [];
-var numMemorized = []
+var numMemorized = [];
 
 /**
  * funzione di controllo numeri memorizzati
  */
 function numbersControl() {
-  for (var i = 0; i < 5; i++){
-    var userNum = parseInt(prompt('inserisci un numero memorizzato'))
-    if (numToMemorize.includes(userNum)) {
-      numMemorized.push(userNum);
+  document.getElementById('instructions').classList.add('visible');
+  //riabilto i campi input
+  for (var i = 1; i <= 5; i++) {
+          document.getElementById(`num${i}`).disabled = false;
+        }
+  document.getElementById('btn-check').addEventListener('click',
+    function () {
+      document.getElementById('instructions').classList.remove('visible');
+      numMemorized = []
+      for (var i = 1; i <= 5; i++){
+        var userNum = parseInt(document.getElementById(`num${i}`).value);
+        if (numToMemorize.includes(userNum) && !numMemorized.includes(userNum)) {
+          numMemorized.push(userNum);
+          document.getElementById(`num${i}`).classList.add('check-ok')
+        } else {
+          document.getElementById(`num${i}`).classList.add('check-ko')
+        }
+      }
+      document.getElementById('guessed1').innerHTML = `Hai indovinato ${numMemorized.length} numeri`
+      // alert('Hai indovinato' + numMemorized.length + 'numeri');
+      // alert('I numeri indovinati sono:' + numMemorized);
     }
-  }
-  alert('Hai indovinato' + numMemorized.length + 'numeri');
-  alert('I numeri indovinati sono:' + numMemorized);
+  )
 
 }
 
-
+// Inizializzo il programma
 //memorizzo 5 numeri casuali in un array
 while (numToMemorize.length < 5) {
   var numRandom = Math.ceil(Math.random() * 100);
@@ -31,12 +46,26 @@ while (numToMemorize.length < 5) {
   }
 }
 
+// scrivo i 5 numeri nei campi input
+for (var i = 0; i < numToMemorize.length; i++){
+  document.getElementById(`num${i+1}`).value = numToMemorize[i]
+}
+
 console.log(numToMemorize);
 
-// Un alert() espone 5 numeri generati casualmente.
-alert('Memorizza questi numeri: ' + numToMemorize);
+//al click di Start
+document.getElementById('btn-start').addEventListener('click',
+  function () {
+    // pulisco i campi input
+    for (var i = 0; i < 5; i++) {
+      document.getElementById(`num${i + 1}`).value = '';
+    }
+    //disabilito i campi input
+    for (var i = 1; i <= 5; i++) {
+      document.getElementById(`num${i}`).disabled = true;
+    }
+    setTimeout(numbersControl, 30000);
 
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-setTimeout(numbersControl, 30000);
-
+  }
+  
+)
